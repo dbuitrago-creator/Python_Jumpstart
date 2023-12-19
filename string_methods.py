f5baf5ef1f98f449f76f39a0699d1f30aa8a0eb9
Isaac Buitrago <mightyking7@gmail.com>
2023-12-18 23:27:17 -0600

    push

---
+++ string_methods.py
@@ -0,0 +1,24 @@
+"""
+Examples to show available strin methods in python
+"""
+
+# Accessing characters in a string
+# Index starts from zero
+
+# Will print out y
+first ="nyc"[1]
+
+print(first)
+# Will print out s
+city ="sfo"
+ft = city[0]
+print(ft)
+
+"""
+len()
+lower()
+upper()
+str()
+"""
+str= "his Is a Mixed Case"
+print(str.lower())
