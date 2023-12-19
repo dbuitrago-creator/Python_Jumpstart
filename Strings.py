5974450e98fa39bf64480825cbdbdfc983212fa9
Diego Buitrago <dbuitrago1999sa@gmail.com>
2023-12-18 23:27:17 -0600

    push

---
+++ Strings.py
@@ -0,0 +1,26 @@
+"""
+Examples to show how strings works in python
+
+Sequence of characters
+Contains a-z , 0-9, @
+In double or single quotes
+"""
+
+a = "This is a simple string"
+b = 'Using single quotes'
+
+print(a)
+print(b)
+
+c = "Need to use 'quotes' inside a string"
+print(c)
+
+#Used backlash to print quotes
+d = "Another way to handle \"quotes\""
+print(d)
+
+# Example on how to seperate a large string with a backlash
+a = ("This is a single\
+   string")
+
+print(a)
---
+++ numbers_operations.py
@@ -0,0 +1,19 @@
+# This is one line comment
+# Exponentiation
+# 10 * 10  ... 20
+exponents = 10 ** 20
+print(exponents)
+
+
+""""
+This is a multi-line comment 
+Modulo - It returns the remainder 
+"""
+
+remainder = 100 % 3
+print(remainder)
+
+print(bool(-1))
+
+print(bool(0))
+
